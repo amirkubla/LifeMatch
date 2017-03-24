f3285632696bdaf291ccdc4c4653c5c7f4ccd480
amir.kublansky <amir.kublansky@toluna.com>
2017-03-24 20:21:40 +0300

    Added MVC solution

---
+++ LifeMatch/LifeMatch/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LifeMatch.MvcApplication" Language="C#" %>
---
+++ LifeMatch/LifeMatch/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LifeMatch.Startup))]
+namespace LifeMatch
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
