b2f78b3ecb4d9989939583adad5fd6ab03972999
amir.kublansky <amir.kublansky@toluna.com>
2017-03-26 22:01:22 +0300

    register form

--- LifeMatch/LifeMatch/Controllers/HomeController.cs
+++ LifeMatch/LifeMatch/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace LifeMatch.Controllers
             return View();
         }
 
+        public ActionResult SearchMatch()
+        {
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
